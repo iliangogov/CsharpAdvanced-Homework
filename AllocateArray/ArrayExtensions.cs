3d9f7a8427ed588e1cf195fa305148a46e481257
iliangogov <iliangogov@gmail.com>
2017-04-20 10:42:36 +0300

    int array extension methods practice

---
+++ AllocateArray/ArrayExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllocateArray
+{
+    public static class ArrayExtensions
+    {
+        internal static long TotalSum(this int[] arr)
+        {
+            int count = arr.Length;
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            long sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                //TODO validate if long limit is reached
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+
+        internal static double AverageValue(this int[] arr)
+        {
+            double count = (double)arr.Length;
+            if(count == 0)
+            {
+                return 0;
+            }
+
+            return arr.Sum() / count;
+        }
+    }
+}
--- AllocateArray/Program.cs
+++ AllocateArray/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace AllocateArray
 {
     class Program
@@ -6,14 +9,26 @@ namespace AllocateArray
         static void Main()
         {
             int count = int.Parse(Console.ReadLine());
-            int[] digitarr= new int[count];
-           
-            for(int i=0;i<digitarr.Length;i++)
+            //var digitarr = new List<int>(count);
+            int[] digitarr = new int[count];
+
+            if (count != 0)
             {
-              digitarr[i] = i * 5;
-                Console.WriteLine(digitarr[i]);
+                for (int i = 0; i < count; i++)
+                {
+                    digitarr[i] = i * 5;
+                    Console.WriteLine(digitarr[i]);
+                }
+
+                Console.WriteLine(digitarr.Average());
+                Console.WriteLine(digitarr.Max());
+                Console.WriteLine(digitarr.Min());
+                Console.WriteLine(digitarr.Sum());
             }
-           
+
+            //ExtensionMethods
+            Console.WriteLine(digitarr.AverageValue());
+            Console.WriteLine(digitarr.TotalSum());
         }
     }
 }
